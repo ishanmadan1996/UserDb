1590f14323f837312a104e012910eeb634aeae93
ishanmadan <ishanmadan1996@gmail.com>
2017-06-25 21:43:11 +0530

    Sends msg to multiple contacts retrieved from sqlite database

--- app/src/main/java/com/example/ishant/realdb/DBOpenHelper.java
+++ app/src/main/java/com/example/ishant/realdb/DBOpenHelper.java
@@ -7,6 +7,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Admin on 11-06-2017.
  */
@@ -16,7 +19,7 @@ public class DBOpenHelper extends SQLiteOpenHelper {
     private static final String LOGTAG = "EXPLORECA";
 
     private static final String DATABASE_NAME = "UserInfo.db";
-    private static final int DATABASE_VERSION = 5;
+    private static final int DATABASE_VERSION = 14;
 
     public static final String TABLE_NAME = "Contact_Info";
     public static final String COLUMN_ID = "Id";
@@ -25,16 +28,17 @@ public class DBOpenHelper extends SQLiteOpenHelper {
     public static final String COLUMN_PHNO = "phno";
     public static final String COLUMN_GROUP = "groups";
 
-    public static final String COLUMN_EMERGENCY = "emergency";
+    public static final String COLUMN_TEAM = "team";
 
     private static final String TABLE =
             "CREATE TABLE " + TABLE_NAME + " (" +
                     COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    COLUMN_NAME + " TEXT, " +
+                    COLUMN_NAME + " TEXT , " +
                     COLUMN_EMAIL + " TEXT, " +
                     COLUMN_PHNO + " TEXT, " +
                     COLUMN_GROUP + " TEXT, " +
-                    COLUMN_EMERGENCY + " TEXT " +
+                    COLUMN_TEAM + " TEXT " +
+
                     ")";
 
 
@@ -58,7 +62,7 @@ public class DBOpenHelper extends SQLiteOpenHelper {
         onCreate(db);
 
     }
-    public boolean insertData (String name,String email,String phno,String groups,String emergency)
+    public boolean insertData (String name,String email,String phno,String groups,String team)
     {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
@@ -66,7 +70,7 @@ public class DBOpenHelper extends SQLiteOpenHelper {
         contentValues.put(COLUMN_EMAIL,email);
         contentValues.put(COLUMN_PHNO,phno);
         contentValues.put(COLUMN_GROUP,groups);
-        contentValues.put(COLUMN_EMERGENCY,emergency);
+        contentValues.put(COLUMN_TEAM,team);
         Long result = db.insert(TABLE_NAME,null,contentValues);
         if(result == -1)
             return  false;
@@ -76,26 +80,54 @@ public class DBOpenHelper extends SQLiteOpenHelper {
 
     public Cursor getAllData() {
         SQLiteDatabase db = this.getWritableDatabase();
+
         Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
         return res;
     }
 
-    public boolean updateData(String id,String name,String email,String phno,String groups,String emergency) {
+    public boolean updateData(String id,String name,String email,String phno,String groups,String team) {
         SQLiteDatabase db = this.getWritableDatabase();
+
         ContentValues contentValues = new ContentValues();
         contentValues.put(COLUMN_ID,id);
         contentValues.put(COLUMN_NAME,name);
         contentValues.put(COLUMN_EMAIL,email);
         contentValues.put(COLUMN_PHNO,phno);
         contentValues.put(COLUMN_GROUP,groups);
-        contentValues.put(COLUMN_EMERGENCY,emergency);
+        contentValues.put(COLUMN_TEAM,team);
         db.update(TABLE_NAME, contentValues, "ID = ?",new String[] { id });
         return true;
     }
-
+    public Cursor showAll(String []string )
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.query(TABLE_NAME, new String[] {COLUMN_ID,COLUMN_NAME,COLUMN_EMAIL,COLUMN_PHNO,COLUMN_GROUP,COLUMN_TEAM},"name=?",string,null,null,null);
+    }
     public Integer deleteData (String id) {
         SQLiteDatabase db = this.getWritableDatabase();
         return db.delete(TABLE_NAME, "ID = ?",new String[] {id});
+    }
+    public List<ContactsConstructor> getAllContacts() {
+        List<ContactsConstructor> contactList = new ArrayList<ContactsConstructor>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + TABLE_NAME;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                ContactsConstructor contact = new ContactsConstructor();
+
+                contact.setPhoneNumber(cursor.getString(3));
+                // Adding contact to list
+                contactList.add(contact);
+            } while (cursor.moveToNext());
+        }
+
+        // return contact list
+        return contactList;
     }
 
 }
--- app/src/main/java/com/example/ishant/realdb/MainActivity.java
+++ app/src/main/java/com/example/ishant/realdb/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.ishant.realdb;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.List;
 
 import static com.example.ishant.realdb.DBOpenHelper.COLUMN_ID;
 import static com.example.ishant.realdb.DBOpenHelper.TABLE_NAME;
@@ -20,18 +28,21 @@ public class MainActivity extends AppCompatActivity {
     SQLiteOpenHelper dbHelper;
     SQLiteDatabase database;
     DBOpenHelper ddb;
-    EditText nm,em,gr,emerg,phno,id;
+    EditText nm,em,gr,team,phno,id;
     Button bt;
     Button btnviewAll;
-    Button btnDelete;
+    Button btnDelete,sms;
 
     Button btnviewUpdate;
     Button edit;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         dbHelper = new DBOpenHelper(this);
         database = dbHelper.getWritableDatabase();
@@ -39,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         nm = (EditText)findViewById(R.id.editText);
         em = (EditText)findViewById(R.id.editText3);
         gr = (EditText)findViewById(R.id.editText4);
-        emerg = (EditText)findViewById(R.id.editText5);
+        team = (EditText)findViewById(R.id.editText5);
         phno = (EditText)findViewById(R.id.editText6);
         id = (EditText)findViewById(R.id.editText7);
         bt = (Button)findViewById(R.id.button) ;
@@ -47,12 +58,68 @@ public class MainActivity extends AppCompatActivity {
         btnDelete = (Button)findViewById(R.id.button4);
         btnviewAll = (Button)findViewById(R.id.button5);
         edit = (Button)findViewById(R.id.button6);
+        sms = (Button)findViewById(R.id.button3);
+        sms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              sendSMSMessage();
+            }
+        });
+
 
         Add();
         viewAll();
         UpdateData();
         DeleteData();
+
     }
+    protected void sendSMSMessage() {
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+        }else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+            SendMsg();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SendMsg();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS failed, please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }
+
+    }
+
+    public void SendMsg() {
+        List<ContactsConstructor> contacts = ddb.getAllContacts();
+        for (ContactsConstructor cn : contacts) {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(cn.getPhoneNumber(), null,"I'm in danger", null, null);
+
+
+        }
+        Toast.makeText(getApplicationContext(), "SMS sent.",
+                Toast.LENGTH_LONG).show();
+    }
+
 
     public void Add () {
         {
@@ -64,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
                             em.getText().toString(),
                             phno.getText().toString(),
                             gr.getText().toString(),
-                            emerg.getText().toString());
+                            team.getText().toString());
                     if(isInserted)
                         Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
                     else
@@ -100,15 +167,17 @@ public class MainActivity extends AppCompatActivity {
 
                     try {
 
-                        final String str = id.getText().toString();
+                        final String  []str = nm.getText().toString().split(" ");
+//                        Intent intent  = new Intent(MainActivity.this.)
 
-                        Cursor res = database.rawQuery("select * from " + TABLE_NAME + " where " + COLUMN_ID + " = " +Integer.parseInt(str)+" ", null);
+                        Cursor res = ddb.showAll(str);
                         if(res != null && res.moveToFirst()){
+                            id.setText(res.getString(0));
                             nm.setText(res.getString(1));
                             em.setText(res.getString(2));
                             phno.setText(res.getString(3));
                             gr.setText(res.getString(4));
-                            emerg.setText(res.getString(5));
+                            team.setText(res.getString(5));
                             res.close();
                         }
 
@@ -131,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
                                     em.getText().toString(),
                                     phno.getText().toString(),
                                     gr.getText().toString(),
-                                    emerg.getText().toString());
+                                    team.getText().toString());
 
 
                             if(isUpdate == true)
@@ -163,7 +232,7 @@ public class MainActivity extends AppCompatActivity {
                                 buffer.append("Email :"+ res.getString(2)+"\n");
                                 buffer.append("PhoneNo :"+ res.getString(3)+"\n");
                                 buffer.append("Group :"+ res.getString(4)+"\n");
-                                buffer.append("Emergency :"+ res.getString(5)+"\n\n");
+                                buffer.append("Team :"+ res.getString(5)+"\n\n");
                             }
 
                             // Show all data
